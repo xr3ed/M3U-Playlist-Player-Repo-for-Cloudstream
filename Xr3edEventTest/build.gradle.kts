98747ca6ee1e8ad206115dfea20b90943367f86e
xr3ed <ikbal.mardhatillah@gmail.com>
2026-07-10 02:00:14 +0700

    feat: tambahkan plugin Xr3edEventTest untuk uji coba perbaikan SBS dan TSN

---
+++ Xr3edEventTest/build.gradle.kts
@@ -0,0 +1,33 @@
+version = 1
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin", "${project.rootDir}/shared/src/main/kotlin")
+    }
+}
+
+cloudstream {
+    language = "id"
+    description = "Plugin Uji Coba live stream Event Piala Dunia 2026"
+    authors = listOf("xr3ed")
+    status = 1
+    tvTypes = listOf(
+        "Live"
+    )
+    iconUrl = "https://raw.githubusercontent.com/xr3ed/Auto-IPTV/main/logo/fifa.png"
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+
+dependencies {
+    val cloudstream by configurations
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+}
