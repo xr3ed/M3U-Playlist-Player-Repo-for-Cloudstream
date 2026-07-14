a40623546e8db05bc9dbc5daa52e13c984420d13
xr3ed <ikbal.mardhatillah@gmail.com>
2026-07-15 01:57:38 +0700

    Bump plugins version

--- Xr3edEvent/build.gradle.kts
+++ Xr3edEvent/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 166
+version = 167
 
 android {
     buildFeatures {
--- xr3edFlix/build.gradle.kts
+++ xr3edFlix/build.gradle.kts
@@ -1,6 +1,6 @@
 import java.util.Properties as JavaProperties
 
-version = 26
+version = 27
 
 android {
     namespace = "com.lagradost.xr3edFlix"
