8aa5ee8dac4e560b54047fe37fee3f0048407e75
xr3ed <xr3ed@users.noreply.github.com>
2026-09-09 23:27:09 +0700

    fix(build): upgrade jvmTarget and java compatibility to 11 to support inlined bytecode from cloudstream pre-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -149,14 +149,14 @@ subprojects {
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
         }
 
 
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
+                jvmTarget.set(JvmTarget.JVM_11)
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
